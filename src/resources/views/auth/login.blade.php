6e6209feaea239eda0ab219703ac364f96ac1b38
khattori1214 <k.hattori@bondstech.jp>
2026-07-31 10:08:56 +0900

    feat: ログイン画面と認証処理を実装

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,30 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=2">
 </head>
 
-<body>
-    <h1>ログイン画面</h1>
+<body class="login-page">
+    <h3>AssetManager
+        -社内資産・備品管理システム-</h3>
+    <h4>ログイン画面</h4>
 
     <form action="/login" method="post">
         @csrf
 
         @error('login')
-        <div class="error"><span>{{ $message }}</span></div>
+            <div class="error"><span>{{ $message }}</span></div>
         @enderror
-        
+
         <label>社員番号ID</label>
+        <br>
         <input type="text" id="employee_no" name="employee_no">
         @error('employee_no')
-        <div class="error"><span>{{ $message }}</span></div>
+            <div class="error"><span>{{ $message }}</span></div>
         @enderror
-    
+
+        <br>
         <label>password</label>
+        <br>
         <input type="password" id="password" name="password">
         @error('password')
-        <div class="error"><span>{{ $message }}</span></div>
+            <div class="error"><span>{{ $message }}</span></div>
         @enderror
-        
+
         <br>
         <input type="submit" value="ログイン">
     </form>
