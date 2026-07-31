fa5e87939778e77c6ef2ecaa2a326d26c0f7a5cf
khattori1214 <k.hattori@bondstech.jp>
2026-07-31 10:12:11 +0900

    feat: トップ画面の期限超過警告とメニューを実装

--- src/resources/views/top/index.blade.php
+++ src/resources/views/top/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>トップ画面</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=2">
 </head>
 
 <body>
@@ -17,20 +18,20 @@
         @endif
     </section>
 
-
-
-    <a href="/histories">
+    <div class="menu-list">
+    <a href="/histories" class="menu-card">
         <span>利用履歴・返却画面</span>
         <span>現在借りている資産や過去の利用履歴を確認し、貸出中の資産を返却します。</span>
     </a>
-    <br>
-    <a href="/assets"><span>資産一覧・申請画面</span>
+    
+    <a href="/assets" class="menu-card"><span>資産一覧・申請画面</span>
         <span>貸出資産の貸出申請や、消耗品の取得申請を行います。</span>
     </a>
-    <br>
-    <a href="/admin"><span>資産登録・在庫管理画面</span>
+    
+    <a href="/admin" class="menu-card"><span>資産登録・在庫管理画面</span>
         <span>貸出資産・消耗品の登録、消耗品の在庫更新、経理連携用CSVのダウンロードを行います。【管理者のみ】</span>
     </a>
+    </div>
 
 </body>
 
