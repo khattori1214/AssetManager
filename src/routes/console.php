394202479af56894891968373f185341aaf5f082
khattori1214 <k.hattori@bondstech.jp>
2026-08-28 11:42:26 +0900

    fix: バッチ処理の時刻を修正

--- src/routes/console.php
+++ src/routes/console.php
@@ -7,13 +7,12 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// 警告メール送信バッチ：毎日10時40分
-Schedule::command('app:send-warning-email')
-    ->dailyAt('11:03');
 
-// 経理連携CSV出力バッチ：毎日10時40分
+Schedule::command('app:send-warning-email')
+    ->dailyAt('11:45');
+
 Schedule::command('app:generate-accounting-csv')
-    ->dailyAt('11:03');
+    ->dailyAt('11:45');
 // Schedule::command('app:generate-accounting-csv')
 //     ->monthlyOn(1, '00:00');
 
